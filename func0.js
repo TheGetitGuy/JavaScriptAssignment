e833c371364fe6cf28bc988755f5a6d71bee5005
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-09-06 04:00:06 -0400

    Fixing some edge cases

--- func0.js
+++ func0.js
@@ -2,6 +2,7 @@ function func0(stringToReverse){
     /**question 1: 1. Write a JavaScript function that reverse a number. 
 Example x = 32243;
 Expected Output: 34223*/ 
+    if(typeof stringToReverse == "number"){stringToReverse = ("" +stringToReverse)}
     return([stringToReverse.split("").reverse().join(""),"is the reversed number"])
 }
 
--- func1.js
+++ func1.js
@@ -3,7 +3,7 @@ function func1(stringToReverse){
 2. Write a JavaScript function that checks whether a passed string is palindrome or not? 
 A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g.,
 madam or nurses run.
-*/ 
+*/  if(typeof stringToReverse == "number"){return[`Please use a ["String"]!`]}
     return([(stringToReverse.split("").reverse().join("")==stringToReverse)? "this is a palindrome" : "this is not a palindrome"])
 }
 
