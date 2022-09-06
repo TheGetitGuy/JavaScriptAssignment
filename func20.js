1178cbd15b77822666b61f3772136e96366b581b
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-09-06 04:51:23 -0400

    up to 20

---
+++ func20.js
@@ -0,0 +1,14 @@
+function func20(arrayCollection, arraySize){
+    /**
+     * 21. Write a JavaScript function to get all possible subset with a fixed length (for example 2)
+combinations in an array. 
+Sample array: [1, 2, 3] and subset length is 2 
+Expected output: [[2, 1], [3, 1], [3, 2]] 
+     */
+    
+    
+
+
+}
+    
+    export {func20}
